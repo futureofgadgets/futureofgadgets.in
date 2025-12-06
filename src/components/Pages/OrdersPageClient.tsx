862d5f99a5c3881231bcad3d70ebeefbcb8fa030
Sonu Rai <sonurai7011@gmail.com>
2025-12-06 13:43:59 +0530

    fix

--- src/components/Pages/OrdersPageClient.tsx
+++ src/components/Pages/OrdersPageClient.tsx
@@ -206,7 +206,7 @@ export default function OrdersPage() {
 
 
   return (
-    <div className="min-h-screen bg-gray-100 mt-1">
+    <div className="min-h-screen bg-gray-100">
       <div className="max-w-7xl mx-auto py-4 px-2 sm:px-4">
         <div className="space-y-4">
           {orders.map((order) => {
